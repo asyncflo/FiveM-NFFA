f5fb104283ee7def12840f83b8119d31137c6240
asyncflo <122392245+asyncflo@users.noreply.github.com>
2025-01-07 16:28:17 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author '59bby' 
+description 'FFA System for FiveM'
+version '1.0.0'
+
+shared_script 'config.lua'
+client_scripts {
+    'client/main.lua'
+}
+server_scripts {
+    'server/main.lua'
+}

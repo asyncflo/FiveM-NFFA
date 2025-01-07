2ecedfbf85172039bda529cabfc35d5d58de48d2
asyncflo <122392245+asyncflo@users.noreply.github.com>
2025-01-07 16:29:21 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,17 @@
+Config = {}
+
+-- Position des Menüs zum Betreten des FFA
+Config.FFAEntryPoint = vector3(1850.0, 2600.0, 45.0) -- Beispiel: Gefängnisbereich
+
+-- Position, an der Spieler im FFA gespawnt werden
+Config.FFASpawnPoint = vector3(1650.0, 2530.0, 45.0)
+
+-- Dimension/Interior für das FFA
+Config.FFADimension = 500 -- Beispiel für eine separate Dimension
+
+-- Waffen, die Spieler im FFA erhalten
+Config.FFAWeapons = {
+    "weapon_bat",
+    "weapon_advancedrifle",
+    "weapon_pistol"
+}

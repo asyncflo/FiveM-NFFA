342c90da06aedde9b6f98e0fce3646423458b9ee
asyncflo <122392245+asyncflo@users.noreply.github.com>
2025-01-07 16:30:36 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,13 +1,18 @@
 Config = {}
 
 -- Position des Menüs zum Betreten des FFA
-Config.FFAEntryPoint = vector3(1850.0, 2600.0, 45.0) -- Beispiel: Gefängnisbereich
+Config.FFAEntryPoint = vector3(1850.0, 2600.0, 45.0)
 
--- Position, an der Spieler im FFA gespawnt werden
-Config.FFASpawnPoint = vector3(1650.0, 2530.0, 45.0)
+-- Zufällige Spawnpunkte innerhalb des FFA
+Config.FFASpawnPoints = {
+    vector3(1650.0, 2530.0, 45.0),
+    vector3(1660.0, 2540.0, 45.0),
+    vector3(1640.0, 2520.0, 45.0),
+    vector3(1655.0, 2535.0, 45.0)
+}
 
 -- Dimension/Interior für das FFA
-Config.FFADimension = 500 -- Beispiel für eine separate Dimension
+Config.FFADimension = 500
 
 -- Waffen, die Spieler im FFA erhalten
 Config.FFAWeapons = {
